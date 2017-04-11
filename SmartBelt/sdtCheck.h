ff6fd550ef838f8d769fe9466761f79be860f13d
Lycoris <lycoris000@126.com>
2017-04-11 20:57:52 +0800

    BIG MODIFICATION ON SDTCHECK
    R.T.

--- SmartBelt/sdtCheck.h
+++ SmartBelt/sdtCheck.h
@@ -1,5 +1,5 @@
 #define SDT_ACCEL_NUM 80
-#define SDT_PRIOR_NUM 80
+#define SDT_PRIOR_NUM 40
 #define SDT_ACCEL_POS_TH 1000
 #define SDT_ACCEL_NEG_TH -500
 #define SDT_ACCEL_INTERVAL 10
@@ -8,65 +8,47 @@ int ipCirculate(int sdtAccelP, int i){
   return (sdtAccelP+i) % SDT_ACCEL_NUM;
 }
 
-bool posthCheck(double accelValue){
-  if(accelValue>SDT_ACCEL_POS_TH)
-    return true;
-  else
-    return false;
-}
-
-bool negthCheck(double accelValue){
-  if(accelValue<SDT_ACCEL_NEG_TH)
-    return true;
-  else
-    return false;
-}
-
-bool intervalCheck(int interval){
-  if(interval<SDT_ACCEL_INTERVAL)
-    return true;
-  else
-    return false;
-}
 bool sdtCheck(double* sdtAccel,int sdtAccelP){
-	bool inposTh=false;
-	bool innegTh=false;
+	int max=0;
+	int min=0;
+	int temp_max=0;
+	int temp_min=0;
 	int interval=SDT_PRIOR_NUM+1;
-	int accelCount=0;
-  for (int j=sdtAccelP+1; j<80; j++){
-    Serial.println(sdtAccel[ipCirculate(sdtAccelP, j)]);
-  }
-  for (int j=0; j<sdtAccelP+1; j++){
-    Serial.println(sdtAccel[ipCirculate(sdtAccelP, j)]);
-  }
-	for (int i=1; i<=40; i++){
-		if(inposTh==false){
-			inposTh=posthCheck(sdtAccel[ipCirculate(sdtAccelP, i)]);
-			if(inposTh==true){
-				accelCount=i;
-				continue;
-			}else{
-				continue;
-			}	
-		}else{
-			innegTh=posthCheck(sdtAccel[ipCirculate(sdtAccelP, i)]);
-			if(innegTh==true){
-				interval=accelCount-i;
-				if(intervalCheck(interval)==true){
-					break;
-				}else{
-					innegTh=false;
-					continue;
-				}
-			}else{
-				continue;
-			}	
-		}		
+	int max_location=0;
+	int min_location=0;
+	for (int j=0; j<80; j++){
+		Serial.println(sdtAccel[ipCirculate(sdtAccelP, j)]);
 	}
-	if(interval>SDT_PRIOR_NUM)
-		return false;
-	else
+	for (int i=1; i<=40; i+=2){
+		if(sdtAccel[ipCirculate(sdtAccelP, i)]<sdtAccel[ipCirculate(sdtAccelP, i+1)]){
+			temp_max=sdtAccel[ipCirculate(sdtAccelP, i+1)];
+			temp_min=sdtAccel[ipCirculate(sdtAccelP, i)];
+			if(temp_max>max){
+				max=temp_max;
+				max_location=i+1;
+			}
+			if(temp_min<min){
+				min=temp_min;
+				min_location=i;				
+			}
+		}else{
+			temp_min=sdtAccel[ipCirculate(sdtAccelP, i+1)];
+			temp_max=sdtAccel[ipCirculate(sdtAccelP, i)];
+			if(temp_max>max){
+				max=temp_max;
+				max_location=i;
+			}
+			if(temp_min<min){
+				min=temp_min;
+				min_location=i+1;				
+			}			
+		}
+	}
+	interval=(max_location-min_location+SDT_PRIOR_NUM) % SDT_PRIOR_NUM;
+	if(interval<=SDT_PRIOR_NUM & max>=SDT_ACCEL_POS_TH & min<=SDT_ACCEL_NEG_TH) 
 		return true;
+	else
+		return false;
 }
 
 
